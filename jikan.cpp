684fd1f3844261164ac7702774be05cac9390350
RealRadOne <shrishtisakshi311@gmail.com>
2020-08-01 14:26:44 +0530

    ToDo Added

--- jikan.cpp
+++ jikan.cpp
@@ -1,23 +1,36 @@
-#include <ctime>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 
-std::string filename = "logs.txt";
-std::string path = "/home/abstractxan/Logs/";
+std::string filename_a = "logs.txt";
+std:: string filename_b= "list.txt";
+std::string path = "E:/ss files/Exxon/";
 int argNum;
 std::vector<std::string> logArgs;
-
-void logWriter() {
+void logWriter() 
+{
   std::ofstream LogFile;
-  std::string filepath = path + filename;
+  std::string filepath;
+  if(logArgs[3]=="Log")
+     filepath= path + filename_a;
+  else
+     filepath= path + filename_b;
   LogFile.open(filepath.c_str(), std::ios::app);
-
-  // UNIX Time
+     // UNIX Time
   std::time_t now = time(0);
-  LogFile << now << " \"" << logArgs[1] << "\" \"" << logArgs[2]<< "\"" << std::endl;
-  std::cout << now << " \"" << logArgs[1] << "\" \"" << logArgs[2]<< "\"" << std::endl;
+  if(logArgs[3]=="Log")
+  {
+    LogFile << localtime(&now)->tm_hour << " \"" << logArgs[1] << "\" \"" << logArgs[2]<< "\"" << std::endl;
+    std::cout <<localtime(&now)->tm_hour << " \"" << logArgs[1] << "\" \"" << logArgs[2]<< "\"" << std::endl;
+    std::cout <<"Task Logged";
+  }
+  if(logArgs[3]=="Todo")
+  {
+    LogFile << localtime(&now) << std::endl;
+    LogFile << logArgs[2] << std::endl; 
+    std::cout<<"Task entered";
+  }
   LogFile.close();
 }
 
@@ -26,17 +39,19 @@ void logger() { logWriter(); }
 void getStats() { std::cout << "Stats feature coming soon " << std::endl; }
 
 void printHelper() {
-  std::cout << "Accepted format: \n$ jikan <heading> <details>\n$ jikan stats\n$ jikan path\n$ jikan file "<< std::endl;
+  std::cout << "Accepted format: \n$ jikan <heading> <details> <Log/Todo>\n$ jikan stats\n$ jikan path\n$ jikan file "<< std::endl;
 }
 void argHandler() {
   std::string command = logArgs[1];
   if (command == "stats") {
     getStats();
   } else if (command == "file" ) {
-    std::cout << path+filename << std::endl;
+    std::cout << "For log" << path+filename_a << std::endl;
+    std::cout << "For TO-DO list"<<path+filename_b<<std::endl;
   }else if (command == "path" ) {
     std::cout << path << std::endl;
-  } else if (argNum == 3) { // Default
+  } else if (argNum == 4) 
+  { // Default 
     logger();
   } else {
     std::cout << command << std::endl;
@@ -46,7 +61,7 @@ void argHandler() {
 
 bool argVectorize(int argc, char *argv[]) {
   argNum = argc;
-  if (argNum < 2) {
+  if (argNum <4) {
     printHelper();
     return false;
   }
